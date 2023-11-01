a64b1f5d6f7726296b2de3e9cee0744ef23013dd
Arne Kreikebaum <Arnekreikebaum@gmail.com>
2023-11-01 20:52:53 +0100

    Corrected the Scrolling behavior of the ListView

--- lib/screens/SettingScreen.dart
+++ lib/screens/SettingScreen.dart
@@ -12,7 +12,7 @@ class SettingScreen extends StatefulWidget {
 class SettingScreenState extends State<SettingScreen> {
   bool _fitnessAndSport = true;
   bool _nutritionAndHealth = true;
-  final TextEditingController _nameController = TextEditingController(text: 'PimmelBirne');
+  final TextEditingController _nameController = TextEditingController(text: 'Arne Kreikebaum');
   DateTime _birthday = DateTime.now();
   String _gender = 'Männlich';
   bool _agbAccepted = true;
@@ -24,184 +24,192 @@ class SettingScreenState extends State<SettingScreen> {
       appBar: AppBar(
         title: const Text('Health Hero'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            /// Section 1: Persönliche Daten
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                'Persönliche Daten',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        children: [
+          /// Section 1: Persönliche Daten
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              'Persönliche Daten',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey.shade300),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: TextField(
-                      controller: _nameController,
-                      decoration: const InputDecoration(
-                        labelText: 'Name',
-                        border: OutlineInputBorder(),
-                      ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey.shade300),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: DropdownButtonFormField<String>(
-                      value: _gender,
-                      items: <String>['Männlich', 'Weiblich', 'Divers'].map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: DropdownButtonFormField<String>(
+                    value: _gender,
+                    items: <String>['Männlich', 'Weiblich', 'Divers'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _gender = newValue!;
+                      });
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Geschlecht',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    title: Text('Geburtsdatum: ${_birthday.toLocal().toIso8601String().substring(0, 10)}'),
+                    trailing: const Icon(Icons.calendar_today),
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: _birthday,
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime.now(),
+                      );
+                      if (pickedDate != null && pickedDate != _birthday) {
                         setState(() {
-                          _gender = newValue!;
+                          _birthday = pickedDate;
                         });
-                      },
-                      decoration: const InputDecoration(
-                        labelText: 'Geschlecht',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
+                      }
+                    },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.zero,
-                      title: Text('Geburtsdatum: ${_birthday.toLocal().toIso8601String().substring(0, 10)}'),
-                      trailing: const Icon(Icons.calendar_today),
-                      onTap: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                          context: context,
-                          initialDate: _birthday,
-                          firstDate: DateTime(1900),
-                          lastDate: DateTime.now(),
-                        );
-                        if (pickedDate != null && pickedDate != _birthday) {
-                          setState(() {
-                            _birthday = pickedDate;
-                          });
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const SizedBox(height: 20), // Spacer
+          ),
+          const SizedBox(height: 20), // Spacer
 
-            // Section 2: Auswahl der Funktionalität
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                'Auswahl der Funktionalität',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
+          // Section 2: Auswahl der Funktionalität
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              'Auswahl der Funktionalität',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            CheckboxListTile(
-              title: const Text('Fitness und Sport'),
-              value: _fitnessAndSport,
+          ),
+          CheckboxListTile(
+            title: const Text('Fitness und Sport'),
+            value: _fitnessAndSport,
+            onChanged: (bool? value) {
+              setState(() {
+                _fitnessAndSport = value!;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: const Text('Ernährung und Gesundheit'),
+            value: _nutritionAndHealth,
+            onChanged: (bool? value) {
+              setState(() {
+                _nutritionAndHealth = value!;
+              });
+            },
+          ),
+          const SizedBox(height: 20), // Spacer
+
+          // Section 3: Datenschutz
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              'Datenschutz',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Checkbox(
+              value: _agbAccepted,
               onChanged: (bool? value) {
                 setState(() {
-                  _fitnessAndSport = value!;
+                  _agbAccepted = value!;
                 });
               },
             ),
-            CheckboxListTile(
-              title: const Text('Ernährung und Gesundheit'),
-              value: _nutritionAndHealth,
+            title: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AGBScreen()),
+                    );
+                  },
+                  child: const Text(
+                    'AGB',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+                const Text('\'s gelesen und akzeptiert'),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Checkbox(
+              value: _dsgvoAccepted,
               onChanged: (bool? value) {
                 setState(() {
-                  _nutritionAndHealth = value!;
+                  _dsgvoAccepted = value!;
                 });
               },
             ),
-            const SizedBox(height: 20), // Spacer
-
-            // Section 3: Datenschutz
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                'Datenschutz',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ),
-            ListTile(
-              leading: Checkbox(
-                value: _agbAccepted,
-                onChanged: (bool? value) {
-                  setState(() {
-                    _agbAccepted = value!;
-                  });
-                },
-              ),
-              title: Row(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const AGBScreen()),
-                      );
-                    },
-                    child: const Text(
-                      'AGBs',
-                      style: TextStyle(color: Colors.blue),
-                    ),
+            title: Row(
+              children: [
+                const Text('Datenverarbeitung gemäß '),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const DSGVOScreen()),
+                    );
+                  },
+                  child: const Text(
+                    'DSGVO',
+                    style: TextStyle(color: Colors.blue),
                   ),
-                  const Text(' gelesen und akzeptiert'),
-                ],
-              ),
+                ),
+              ],
             ),
-            ListTile(
-              leading: Checkbox(
-                value: _dsgvoAccepted,
-                onChanged: (bool? value) {
-                  setState(() {
-                    _dsgvoAccepted = value!;
-                  });
-                },
-              ),
-              title: Row(
-                children: [
-                  const Text('Datenverarbeitung gemäß '),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const DSGVOScreen()),
-                      );
-                    },
-                    child: const Text(
-                      'DSGVO',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20), // Spacer
+          ),
+          const SizedBox(height: 20), // Spacer
 
-            // Save Button
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Save the changes to the database
-              },
-              child: const Text('Speichern'),
-            ),
-          ],
-        ),
+          // Save Button
+          ElevatedButton(
+            onPressed: () {
+              // TODO: Save the changes to the database
+            },
+            child: const Text('Speichern'),
+          ),
+          
+          const SizedBox(height: 5), // Spacer
+
+          // Save Button
+          ElevatedButton(
+            onPressed: () {
+              // TODO: Save the changes to the database
+            },
+            child: const Text('Alle Quizzes und Challenges zurücksetzten'),
+          ),
+        ],
       ),
     );
   }
