ae61abcf382da84c80e18bf04279a4887b189c44
Arne Kreikebaum <Arnekreikebaum@gmail.com>
2023-11-06 20:21:55 +0100

    Revert "Feature/arne 05"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:healthhero/theme/theme.dart';
 import 'screens/LearnScreen.dart';
 import 'screens/ChallengeScreen.dart';
 import 'screens/QuizScreen.dart';
@@ -21,9 +20,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Health Hero',
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: const Color.fromARGB(255, 33, 243, 191),
+        primarySwatch: const MaterialColor(0xFF21F3BF, {
+          50: Color(0xFFE0F7F2),
+          100: Color(0xFFB3F0E4),
+          200: Color(0xFF80E9D4),
+          300: Color(0xFF4DE2C5),
+          400: Color(0xFF26DDB9),
+          500: Color(0xFF21F3BF),
+          600: Color(0xFF0FC29A),
+          700: Color(0xFF0A9B7A),
+          800: Color(0xFF07735A),
+          900: Color(0xFF055D46),
+        }),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color?>(const Color.fromARGB(255, 33, 243, 191)),
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color?>(const Color.fromARGB(255, 33, 243, 191)),
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       home: const MyBottomNavigationBar(),
     );
   }
@@ -55,8 +83,19 @@ class MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).elevatedButtonTheme.style?.backgroundColor?.resolve({}) ?? Colors.white,
+        title: const Text('HEALTH HERO'),
+      ),
       body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        selectedIconTheme: const IconThemeData(color: Colors.white),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
         onTap: onTabTapped,
         currentIndex: _currentIndex,
         items: const [
@@ -74,11 +113,11 @@ class MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.emoji_events),
-            label: 'Statistik',
+            label: 'Achievements',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
-            label: 'Einstellungen',
+            label: 'Settings',
           ),
         ],
       ),
