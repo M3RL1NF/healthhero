11a1c1429d4995648819f025633913d8a7dda3a9
Arne Kreikebaum <Arnekreikebaum@gmail.com>
2023-11-07 23:46:56 +0100

    added Styling to Lernzenturm

--- lib/screens/LearnScreen.dart
+++ lib/screens/LearnScreen.dart
@@ -2,23 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:healthhero/data/ChallengeHelper.dart';
 import 'package:healthhero/models/challenge.dart';
 
+
 class LearnScreen extends StatefulWidget {
   const LearnScreen({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _LearnScreenState createState() => _LearnScreenState();
 }
 
 class _LearnScreenState extends State<LearnScreen> {
   late List<Challenge> challenges = [];
   late String selectedCategory;
-
+  final Map<String, String> categoryDisplayNames = {
+    'Sport': 'Sport',
+    'Nutrition': 'Ernährung',
+  };
+  
   @override
   void initState() {
     super.initState();
+    selectedCategory =
+        "Sport"; // The initial category must match one of the keys in the mapping
     fetchChallenges();
-    selectedCategory = "Sport";
   }
 
   Future<void> fetchChallenges() async {
@@ -31,56 +36,81 @@ class _LearnScreenState extends State<LearnScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lernzentrum'),
-        titleTextStyle: const TextStyle(
-          color: Color.fromARGB(255, 255, 255, 255),
-          fontWeight: FontWeight.bold,
-        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Theme.of(context).primaryColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 8, 4, 8),
+                child: DropdownButton<String>(
+                  value: selectedCategory,
+                  items: categoryDisplayNames.entries.map((entry) {
+                    return DropdownMenuItem<String>(
+                      value: entry.key,
+                      child: Text(entry.value),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      if (newValue != null) {
+                        selectedCategory = newValue;
+                        fetchChallenges();
+                      }
+                    });
+                  },
+                  underline: Container(),
+                  icon: const Icon(Icons.arrow_drop_down, color: Colors.white),
+                  dropdownColor: Theme.of(context).primaryColor,
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       body: Column(
         children: [
-          DropdownButton<String>(
-            value: selectedCategory,
-            items: <String>['Sport', 'Nutrition'].map((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: (String? newValue) {
-              setState(() {
-                selectedCategory = newValue!;
-              });
-            },
-          ),
           Expanded(
-            child: ListView(
-              children: challenges
-                  .where((challenge) => challenge.category == selectedCategory)
-                  .map((challenge) {
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.all(0),
-                      title: Text(
-                        challenge.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.only(top: 8.0),
-                        child: Text(
-                          challenge.description,
-                          textAlign: TextAlign.justify,
-                        ),
+            child: ListView.builder(
+              itemCount: challenges.length,
+              itemBuilder: (context, index) {
+                final challenge = challenges[index];
+                if (challenge.category == selectedCategory) {
+                  return Card(
+                    color: Colors.black,
+                    elevation: 0,
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.all(12.0), // Padding inside the card
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Align text to the start of the cardS
+                        children: [
+                          Text(
+                            challenge.title,
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                          const SizedBox(
+                              height:
+                                  8.0), // Space between title and description
+                          Text(
+                            challenge.description,
+                            textAlign: TextAlign.justify,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                );
-
-              }).toList(),
+                  );
+                } else {
+                  return Container();
+                }
+              },
             ),
           ),
         ],
